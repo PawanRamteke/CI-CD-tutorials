cb1647f29c9642a0e972a7955aaf5f95fd110e0b
Pawan Ramteke <pawanramteke7@gmail.com>
2025-09-24 19:22:31 +0530

    project added

---
+++ CI-CD Tutorials/CI_CD_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_TutorialsApp.swift
+//  CI-CD Tutorials
+//
+//  Created by Pawan's Macbook Air on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI-CD TutorialsUITests/CI_CD_TutorialsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CI_CD_TutorialsUITestsLaunchTests.swift
+//  CI-CD TutorialsUITests
+//
+//  Created by Pawan's Macbook Air on 24/09/25.
+//
+
+import XCTest
+
+final class CI_CD_TutorialsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
